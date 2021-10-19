71c7c70cd95cf3b6e667dc8dae3761c2aa5c1524
jjwkang08 <jjjw8989@naver.com>
2021-10-19 15:26:06 +0900

    로그인 스토리지 업로드

--- back/models/auth/delete-user.js
+++ back/models/auth/delete-user.js
@@ -3,21 +3,36 @@ const { pool } = require('../../modules/mysql-init')
 const { findPasswd } = require('./find-user')
 
 const deleteUser = async (user) => {
-	let sql;
+	let sql, allow = false;
 	try {
-		const { idx, passwd, msg } = user
-		const { success } = await findPasswd(idx, passwd)
-		if(success) {
+		const { idx, passwd, msg, status } = user
+
+		if(status === '3') {	// SNS 회원 처리
 			sql = " UPDATE users SET status = '0' WHERE idx=? "
 			const [r] = await pool.execute(sql, [idx])
 			if(r.affectedRows) {
-				sql = " INSERT INTO users_withdrawal SET fidx=?, msg=? "
-				const [r2] = await pool.execute(sql, [idx, msg])
-				return r2.affectedRows
-					? { success: true }
-					: { success: false }
+				sql = " UPDATE users_sns SET status = '0' WHERE fidx=? "
+				const [r2] = await pool.execute(sql, [idx])
+				allow = r2.affectedRows
 			}
 			else return { success: false }
+		}
+		else {	// 일반 회원 처리
+			const { success } = await findPasswd(idx, passwd)
+			if(success) {
+				sql = " UPDATE users SET status = '0' WHERE idx=? "
+				const [r2] = await pool.execute(sql, [idx])
+				allow = r2.affectedRows
+			}
+			else return { success: false }
+		}
+
+		if(allow) {
+			sql = " INSERT INTO users_withdrawal SET fidx=?, msg=? "
+			const [r3] = await pool.execute(sql, [idx, msg])
+			return r3.affectedRows
+				? { success: true }
+				: { success: false }
 		}
 		else return { success: false }
   }
