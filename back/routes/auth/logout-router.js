1522d7b3a280b065ffdce8c2e83920da97a33644
jjwkang08 <jjjw8989@naver.com>
2021-10-19 17:52:36 +0900

    수정 및 업로드

--- back/routes/auth/logout-router.js
+++ back/routes/auth/logout-router.js
@@ -6,7 +6,7 @@ const { pool } = require('../../modules/mysql-init')
 const { isUser, isGuest } = require('../../middlewares/auth-mw')
 
 router.get('/', isUser, (req, res, next) => {
-	req.logOut()
+	req.logout()
 	req.session.destroy()
 	res.locals.user = null
 	res.send(alert('로그아웃 되었습니다.'))
--- back/routes/auth/withdrawal-router.js
+++ back/routes/auth/withdrawal-router.js
@@ -22,7 +22,7 @@ router.post('/', isUser, async (req, res, next) => {
     const user = { ...req.body, idx: req.user.idx, status: req.user.status }
 		const { success } = await deleteUser(user)
     if(success) {
-      req.logOut()
+      req.logout()
       req.session.destroy()
       res.locals.user = null
       res.send(alert(ALERT.WITHDRAWAL))
